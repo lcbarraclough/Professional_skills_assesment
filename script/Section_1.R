05cef7ccd3705b1a0ec029d26535119d0ff688a8
BARRACLOUGH Laura <s1729795@ed.ac.uk>
2021-11-19 15:18:21 +0000

    Finished exercise 1

--- script/Section_1.R
+++ script/Section_1.R
@@ -12,12 +12,14 @@ inga<- read.csv("data/Inga_traits.csv")
 
 # PART a: make a histogram for leaf area 
 (leaf_hist<- ggplot(inga, aes(x=Leaf_Area)) +
-  geom_histogram(binwidth = 250, colour= "#9400D3", fill= "#00EE00") +
+  geom_histogram(bins = 40, colour= "#9400D3", fill= "#00EE00") +
     theme_bw() +
     ylab("Count\n") +
     xlab("\nLeaf Area"))
+ggsave(leaf_hist, filename = "graphs/histogram_1.png")
 
 (leaf_hist_2<- hist(inga$Leaf_Area, breaks = 37))
+summary(inga$Leaf_Area)
   
 # PART b
 inga<- mutate(inga, log(Leaf_Area)) #log leaf areas
